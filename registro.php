6a9b65eb3d4d2cb594c60ec47bb9688470297d42
GatoBuchon <133423010+GatoBuchon@users.noreply.github.com>
2024-06-10 09:30:47 -0600

    Add files via upload

---
+++ registro.php
@@ -0,0 +1,33 @@
+<?php
+// Datos de conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "1234567890";
+$dbname = "usuarios_db";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener datos del formulario
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+
+
+// Insertar datos en la base de datos
+$sql = "INSERT INTO usuarios (username, password) VALUES ('$user', '$pass')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Registro exitoso";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Cerrar conexión
+$conn->close();
+?>
